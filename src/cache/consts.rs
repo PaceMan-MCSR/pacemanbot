10fa1e8c5cc3971e1895c149a4759db2de76f2b1
sathya-pramodh <sathyapramodh17@gmail.com>
2025-03-02 12:16:35 +0530

    chore: Change pacemanbot channel.

--- src/cache/consts.rs
+++ src/cache/consts.rs
@@ -1,4 +1,4 @@
 pub const ROLE_PREFIX: &'static str = "*115";
-pub const PACEMANBOT_CHANNEL: &'static str = "pacemanbot-other-versions";
+pub const PACEMANBOT_CHANNEL: &'static str = "other-versions";
 pub const PACEMANBOT_RUNNER_NAMES_CHANNEL: &'static str = "pacemanbot-runner-names-115";
 pub const PACEMANBOT_RUNNER_LEADERBOARD_CHANNEL: &'static str = "pacemanbot-runner-leaderboard-115";
