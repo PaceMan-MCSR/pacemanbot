b54c8979e4719c94ee100814906c96a8e25a3474
Saanvi <saanviwastaken544@gmail.com>
2025-06-22 16:34:32 +0530

    revert: previous commit

--- tests/test-one.sh
+++ tests/test-one.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -X POST -H "Content-Type: application/json" -d "$(cat $1)" http://host.docker.internal:8001/api/ars/sendevent
+curl -X POST -H "Content-Type: application/json" -d "$(cat $1)" http://localhost:8001/api/ars/sendevent
 
