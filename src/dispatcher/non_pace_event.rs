6797df20cf03c03ad01c8424ea2d596314f25435
its-saanvi <saanviwastaken544@gmail.com>
2025-06-28 23:29:49 +0530

    chore: run fmt

--- src/dispatcher/non_pace_event.rs
+++ src/dispatcher/non_pace_event.rs
@@ -2,110 +2,141 @@ use std::sync::Arc;
 
 use serenity::{builder::CreateEmbedAuthor, client::Context};
 
-use crate::{cache::{consts::PACEMANBOT_RUNNER_LEADERBOARD_CHANNEL, guild_data::GuildData}, eprintln, utils::{format_time::format_time, millis_to_mins_secs::millis_to_mins_secs, update_leaderboard::update_leaderboard}, ws::response::{Event, Response}};
+use crate::{
+    cache::{consts::PACEMANBOT_RUNNER_LEADERBOARD_CHANNEL, guild_data::GuildData},
+    eprintln,
+    utils::{
+        format_time::format_time, millis_to_mins_secs::millis_to_mins_secs,
+        update_leaderboard::update_leaderboard,
+    },
+    ws::response::{Event, Response},
+};
 
-use super::consts::{CREDITS_EMOJI, OFFLINE_EMOJI, PEARL_EMOJI, ROD_EMOJI, SPECIAL_UNDERSCORE, TWITCH_EMOJI};
+use super::consts::{
+    CREDITS_EMOJI, OFFLINE_EMOJI, PEARL_EMOJI, ROD_EMOJI, SPECIAL_UNDERSCORE, TWITCH_EMOJI,
+};
 
-pub async fn handle_non_pace_event(ctx: Arc<Context>, response: &Response, live_link: String, stats_link: String, author: CreateEmbedAuthor, last_event: &Event, guild_data: &mut GuildData) {
-        let player_data = match guild_data.players.get_mut(&response.nickname.to_lowercase()) {
-            Some(data) => data,
-            None => {
-                return println!(
+pub async fn handle_non_pace_event(
+    ctx: Arc<Context>,
+    response: &Response,
+    live_link: String,
+    stats_link: String,
+    author: CreateEmbedAuthor,
+    last_event: &Event,
+    guild_data: &mut GuildData,
+) {
+    let player_data = match guild_data
+        .players
+        .get_mut(&response.nickname.to_lowercase())
+    {
+        Some(data) => data,
+        None => {
+            return println!(
                     "Skipping guild because player name: {} is not in the runners channel for guild name: {}", 
                     response.nickname, 
                     guild_data.name
                 );
-            }
-        };
+        }
+    };
 
-        let runner_name = response.nickname.to_owned();
-        let (minutes, seconds) = millis_to_mins_secs(last_event.igt as u64);
-    
-        let finish_minutes = match player_data.finish {
-            Some(mins) => mins,
-            None => {
-                if !guild_data.is_private && minutes >= 10 {
-                    return println!(
+    let runner_name = response.nickname.to_owned();
+    let (minutes, seconds) = millis_to_mins_secs(last_event.igt as u64);
+
+    let finish_minutes = match player_data.finish {
+        Some(mins) => mins,
+        None => {
+            if !guild_data.is_private && minutes >= 10 {
+                return println!(
                         "Skipping guild name: {} because it is not a sub 10 completion and the guild is public.", 
                         guild_data.name
                     );
-                }
-                // minutes + 1 will always be greater than minutes. 
-                // This is done to send finish message always if finish time is not defined.
-                minutes + 1
-            }, 
-        };
-        if minutes >= finish_minutes {
-            return println!(
-                "Skipping guild name: {} because finish time is above the defined amount.",
-                guild_data.name,
-            )
+            }
+            // minutes + 1 will always be greater than minutes.
+            // This is done to send finish message always if finish time is not defined.
+            minutes + 1
         }
-
-        let finish_content = format!(
-            "{}  {} - Finish", 
-            CREDITS_EMOJI,
-            format_time(last_event.igt as u64),
+    };
+    if minutes >= finish_minutes {
+        return println!(
+            "Skipping guild name: {} because finish time is above the defined amount.",
+            guild_data.name,
         );
+    }
 
-        let mut item_data_content = format!("{} {}", ROD_EMOJI, 0);
-        item_data_content = format!("{}  {} {}", item_data_content, PEARL_EMOJI, 0);
+    let finish_content = format!(
+        "{}  {} - Finish",
+        CREDITS_EMOJI,
+        format_time(last_event.igt as u64),
+    );
 
-        match guild_data.pace_channel.send_message(&ctx, |m| {
+    let mut item_data_content = format!("{} {}", ROD_EMOJI, 0);
+    item_data_content = format!("{}  {} {}", item_data_content, PEARL_EMOJI, 0);
+
+    match guild_data
+        .pace_channel
+        .send_message(&ctx, |m| {
             m.embed(|e| {
                 e.set_author(author);
                 e.field(finish_content, "", false);
-                    if response.user.live_account.is_some() {
-                        e.field(format!("{} {}", TWITCH_EMOJI, live_link.clone()), "", false);
-                    } else {
-                        e.field(format!("{}  Offline", OFFLINE_EMOJI), "", false);
-                    }
+                if response.user.live_account.is_some() {
+                    e.field(format!("{} {}", TWITCH_EMOJI, live_link.clone()), "", false);
+                } else {
+                    e.field(format!("{}  Offline", OFFLINE_EMOJI), "", false);
+                }
                 e.field("Splits", format!("[Link]({})", stats_link), true);
-                e.field("Time", format!("<t:{}:R>", (response.last_updated / 1000) as u64), true);
+                e.field(
+                    "Time",
+                    format!("<t:{}:R>", (response.last_updated / 1000) as u64),
+                    true,
+                );
                 e.field("Items", item_data_content.clone(), true);
                 e
             })
-        }).await {
-            Ok(_) => {
-                println!(
-                    "Sent pace-ping for user with name: '{}' for split: 'Finish' in guild name: {}.",
-                    response.nickname, guild_data.name 
-                );
-            }
-            Err(err) => {
-                return eprintln!(
-                    "HandleNonPaceEvent: send split: 'Finish' due to: {}",
-                    err
-                );
-            }
-        };
+        })
+        .await
+    {
+        Ok(_) => {
+            println!(
+                "Sent pace-ping for user with name: '{}' for split: 'Finish' in guild name: {}.",
+                response.nickname, guild_data.name
+            );
+        }
+        Err(err) => {
+            return eprintln!("HandleNonPaceEvent: send split: 'Finish' due to: {}", err);
+        }
+    };
 
-        if !guild_data.is_private || guild_data.lb_channel.is_none() {
-            return println!(
+    if !guild_data.is_private || guild_data.lb_channel.is_none() {
+        return println!(
                 "Can't handle non pace event for guild name: {} because it is a public server or does not have a leaderboard channel.", 
                 guild_data.name
             );
-        }
+    }
 
-        match update_leaderboard(&ctx, guild_data.lb_channel.unwrap(), runner_name.to_owned().replace("_", SPECIAL_UNDERSCORE), (minutes, seconds))
-            .await
-        {
-            Ok(_) => {
-                println!(
-                    "Updated leaderboard in #{} for guild name: {}, runner name: {} with time: {}.", 
-                    PACEMANBOT_RUNNER_LEADERBOARD_CHANNEL,
-                    guild_data.name, 
-                    runner_name, 
-                    format_time(last_event.igt as u64),
-                );
-            }
-            Err(err) => {
-                eprintln!(
+    match update_leaderboard(
+        &ctx,
+        guild_data.lb_channel.unwrap(),
+        runner_name.to_owned().replace("_", SPECIAL_UNDERSCORE),
+        (minutes, seconds),
+    )
+    .await
+    {
+        Ok(_) => {
+            println!(
+                "Updated leaderboard in #{} for guild name: {}, runner name: {} with time: {}.",
+                PACEMANBOT_RUNNER_LEADERBOARD_CHANNEL,
+                guild_data.name,
+                runner_name,
+                format_time(last_event.igt as u64),
+            );
+        }
+        Err(err) => {
+            eprintln!(
                     "HandleNonPaceEvent: update leaderboard in guild name: {} for runner name: {} due to: {}",
                     guild_data.name,
                     response.nickname.to_owned(), 
                     err
                 );
-            }
-        };
+        }
+    };
 }
--- src/dispatcher/pace_event.rs
+++ src/dispatcher/pace_event.rs
@@ -2,208 +2,264 @@ use std::sync::Arc;
 
 use serenity::{builder::CreateEmbedAuthor, client::Context, prelude::Mentionable};
 
-use crate::{cache::{guild_data::GuildData, players::PlayerSplitsData, split::{Split, Structure}}, eprintln, utils::{format_time::format_time, millis_to_mins_secs::millis_to_mins_secs}, ws::response::{Event, Item, Response}};
+use crate::{
+    cache::{
+        guild_data::GuildData,
+        players::PlayerSplitsData,
+        split::{Split, Structure},
+    },
+    eprintln,
+    utils::{format_time::format_time, millis_to_mins_secs::millis_to_mins_secs},
+    ws::response::{Event, Item, Response},
+};
 
-use super::{consts::{OFFLINE_EMOJI, PEARL_EMOJI, ROD_EMOJI, SPECIAL_UNDERSCORE, TWITCH_EMOJI}, get_run_info::get_run_info, run_info::RunType};
+use super::{
+    consts::{OFFLINE_EMOJI, PEARL_EMOJI, ROD_EMOJI, SPECIAL_UNDERSCORE, TWITCH_EMOJI},
+    get_run_info::get_run_info,
+    run_info::RunType,
+};
 
-pub async fn handle_pace_event(ctx: Arc<Context>, response: &Response, live_link: String, stats_link: String, author: CreateEmbedAuthor, last_event: &Event, guild_data: &mut GuildData) 
-{
-        let run_info = 
-            match get_run_info(response, last_event) {
-                Some(info) => info,
-                None => {
-                    return eprintln!("HandlePaceEvent: Unrecognized event id: {:#?}.", last_event.event_id);
-                }
-            };
+pub async fn handle_pace_event(
+    ctx: Arc<Context>,
+    response: &Response,
+    live_link: String,
+    stats_link: String,
+    author: CreateEmbedAuthor,
+    last_event: &Event,
+    guild_data: &mut GuildData,
+) {
+    let run_info = match get_run_info(response, last_event) {
+        Some(info) => info,
+        None => {
+            return eprintln!(
+                "HandlePaceEvent: Unrecognized event id: {:#?}.",
+                last_event.event_id
+            );
+        }
+    };
 
-        let player_data = match guild_data.players.get_mut(&response.nickname.to_lowercase()) {
-            Some(data) => data,
-            None => {
-                if guild_data.is_private {
-                    return println!(
+    let player_data = match guild_data
+        .players
+        .get_mut(&response.nickname.to_lowercase())
+    {
+        Some(data) => data,
+        None => {
+            if guild_data.is_private {
+                return println!(
                         "Skipping guild because player name: {} is not in the runners channel for guild name: {}", 
                         response.nickname, 
                         guild_data.name
                     );
-                }
-                let player_data = PlayerSplitsData::default();
-                guild_data.players.insert(response.nickname.to_owned().to_lowercase(), player_data);
-                guild_data.players.get_mut(&response.nickname.to_lowercase()).unwrap()
             }
-        };
-        let split_desc = match run_info.split.desc(&run_info.structure) {
-            Some(desc) => desc,
-            None => {
-                return eprintln!("HandlePaceEvent: get split desc for split: {:#?}", run_info.split);
-            }
-        };
-
-        let split_emoji = match run_info.split.get_emoji(&run_info.structure) {
-            Some(emoji) => emoji,
-            None => {
-                return eprintln!("HandlePaceEvent: get split emoji for split: {:#?}", run_info.split);
-            }
-        };
-
-        let roles_to_ping = guild_data.roles
-            .iter()
-            .filter(|role| {
-                let (split_minutes, split_seconds) = millis_to_mins_secs(last_event.igt as u64);
-                if role.guild_role.name.contains("PB") {
-                    if !guild_data.is_private {
-                        return false;
-                    }
-                    let pb_minutes = player_data.get(&role.split).unwrap().to_owned();
-                    role.split == run_info.split && pb_minutes > split_minutes
-                } else if role.guild_role.name.contains("+") {
-                    role.split == run_info.split
-                        && role.runner.to_lowercase() == response.nickname.to_lowercase()
-                        && role.minutes >= split_minutes
-                        && (role.minutes != split_minutes || role.seconds > split_seconds)
-                } else {
-                    role.split == run_info.split
-                        && role.minutes >= split_minutes
-                        && (role.minutes != split_minutes || role.seconds > split_seconds)
-                }
-            })
-            .collect::<Vec<_>>();
-
-        if roles_to_ping.is_empty() {
-            return println!(
-                "Skipping split: '{}' because there are no roles to ping in guild name: {}.",
-                split_desc, guild_data.name 
+            let player_data = PlayerSplitsData::default();
+            guild_data
+                .players
+                .insert(response.nickname.to_owned().to_lowercase(), player_data);
+            guild_data
+                .players
+                .get_mut(&response.nickname.to_lowercase())
+                .unwrap()
+        }
+    };
+    let split_desc = match run_info.split.desc(&run_info.structure) {
+        Some(desc) => desc,
+        None => {
+            return eprintln!(
+                "HandlePaceEvent: get split desc for split: {:#?}",
+                run_info.split
             );
         }
+    };
 
-        let live_indicator = if response.user.live_account.is_some() {
-            "🔴"
-        } else {
-            "⚪"
-        };
-
-        let mut item_data_content = String::new();
-
-        match &response.item_data {
-            Some(data) => {
-                let pearl_count = data.estimated_counts.get(&Item::MinecraftEnderPearl).unwrap_or(&0).to_string();
-                let mut rod_count = data.estimated_counts.get(&Item::MinecraftBlazeRod).unwrap_or(&0).to_string();
-                if let Some(Structure::Bastion) = run_info.structure {
-                        if rod_count == String::from("0") && run_info.split == Split::SecondStructure {
-                            rod_count = String::from("1+");
-                        }
-                }
-                if item_data_content == "" {
-                    item_data_content = format!("{} {}", ROD_EMOJI, rod_count);
-                } else {
-                    item_data_content = format!("{}  {} {}", item_data_content, ROD_EMOJI, rod_count);
-                }
-                if item_data_content == "" {
-                    item_data_content = format!("{} {}", PEARL_EMOJI, pearl_count);
-                } else {
-                    item_data_content = format!("{}  {} {}", item_data_content, PEARL_EMOJI, pearl_count);
-                }
-            },
-            None => {
-                item_data_content = format!("{} {}", ROD_EMOJI, 0);
-                item_data_content = format!("{}  {} {}", item_data_content, PEARL_EMOJI, 0);
-            },
+    let split_emoji = match run_info.split.get_emoji(&run_info.structure) {
+        Some(emoji) => emoji,
+        None => {
+            return eprintln!(
+                "HandlePaceEvent: get split emoji for split: {:#?}",
+                run_info.split
+            );
         }
-        let metadata = format!(
-            "{} {} - {} {}", 
-            live_indicator,
-            format_time(last_event.igt as u64), 
-            split_desc, 
-            response.nickname.replace("_", SPECIAL_UNDERSCORE)
-        );
+    };
 
-        let ping_content = format!(
-            "{}\n-# {}",
-            metadata.clone(),
-            roles_to_ping
-                .iter()
-                .map(|role| role.guild_role.mention().to_string())
-                .collect::<Vec<_>>()
-                .join(" "),
-        );
-
-
-        let pace_content = format!(
-            "{}  {} - {}",
-            split_emoji,
-            format_time(last_event.igt as u64),
-            split_desc,
-        );
-
-        match guild_data.pace_channel.send_message(
-            &ctx, 
-            |m| {
-                m.embed(|e| {
-                    e.set_author(author.clone());
-                    e.field(pace_content.clone(), "", false);
-                    if response.user.live_account.is_some() {
-                        e.field(format!("{} {}", TWITCH_EMOJI, live_link.clone()), "", false);
-                    } else {
-                        e.field(format!("{}  Offline", OFFLINE_EMOJI), "", false);
-                    }
-                    e.field("Splits", format!("[Link]({})", stats_link.clone()), true);
-                    e.field("Time", format!("<t:{}:R>", (response.last_updated / 1000) as u64), true);
-                    e.field("Items", item_data_content.clone(), true);
-                    if let RunType::Bastionless = run_info.run_type {
-                        e.field("Bastionless", "Yes", true);
-                    }                    
-                    e
-                })
-                .content(ping_content.to_owned())
-            }).await {
-            Ok(mut message) => {
-                println!(
-                    "Sent pace-ping for user with name: '{}' for split: '{}' in guild name: {}.",
-                    response.nickname, split_desc, guild_data.name 
-                );
-                let removable_roles = roles_to_ping
-                    .iter()
-                    .filter(|r| r.runner.as_str() != "")
-                    .map(|r| r.guild_role.mention())
-                    .collect::<Vec<_>>();
-                let mut new_content = ping_content.to_owned();
-                for role in removable_roles {
-                    let replacable_str = format!("{} ", role);
-                    new_content = new_content.replace(replacable_str.as_str(), "");
+    let roles_to_ping = guild_data
+        .roles
+        .iter()
+        .filter(|role| {
+            let (split_minutes, split_seconds) = millis_to_mins_secs(last_event.igt as u64);
+            if role.guild_role.name.contains("PB") {
+                if !guild_data.is_private {
+                    return false;
                 }
-                let content_removed_metadata = new_content.replace(format!("{}\n", metadata).as_str(), ""); 
-                match message.edit(
-                    &ctx.http, 
-                    |m| {
-                        m.embed(|e| {
-                            e.set_author(author);
-                            e.field(pace_content, "", false);
-                            if response.user.live_account.is_some() {
-                                e.field(format!("{} {}", TWITCH_EMOJI, live_link.clone()), "", false);
-                            } else {
-                                e.field(format!("{}  Offline", OFFLINE_EMOJI), "", false);
-                            }
-                            e.field("Splits", format!("[Link]({})", stats_link), true);
-                            e.field("Time", format!("<t:{}:R>", (response.last_updated / 1000) as u64), true);
-                            e.field("Items", item_data_content, true);
-                            if let RunType::Bastionless = run_info.run_type {
-                                e.field("Bastionless", "Yes", true);
-                            }
-                            e
-                        })
-                        .content(content_removed_metadata)
-                    }).await {
-                    Ok(_) => (),
-                    Err(err) => {
-                        return eprintln!("HandlePaceEvent: edit message due to: {}", err);
-                    } 
-                };
+                let pb_minutes = player_data.get(&role.split).unwrap().to_owned();
+                role.split == run_info.split && pb_minutes > split_minutes
+            } else if role.guild_role.name.contains("+") {
+                role.split == run_info.split
+                    && role.runner.to_lowercase() == response.nickname.to_lowercase()
+                    && role.minutes >= split_minutes
+                    && (role.minutes != split_minutes || role.seconds > split_seconds)
+            } else {
+                role.split == run_info.split
+                    && role.minutes >= split_minutes
+                    && (role.minutes != split_minutes || role.seconds > split_seconds)
             }
-            Err(err) => {
-                return eprintln!(
-                    "HandlePaceEvent: send split: '{}' with roles: {:?} due to: {}",
-                    split_desc, roles_to_ping, err
-                );
-            }
-        };
-}
+        })
+        .collect::<Vec<_>>();
 
+    if roles_to_ping.is_empty() {
+        return println!(
+            "Skipping split: '{}' because there are no roles to ping in guild name: {}.",
+            split_desc, guild_data.name
+        );
+    }
+
+    let live_indicator = if response.user.live_account.is_some() {
+        "🔴"
+    } else {
+        "⚪"
+    };
+
+    let mut item_data_content = String::new();
+
+    match &response.item_data {
+        Some(data) => {
+            let pearl_count = data
+                .estimated_counts
+                .get(&Item::MinecraftEnderPearl)
+                .unwrap_or(&0)
+                .to_string();
+            let mut rod_count = data
+                .estimated_counts
+                .get(&Item::MinecraftBlazeRod)
+                .unwrap_or(&0)
+                .to_string();
+            if let Some(Structure::Bastion) = run_info.structure {
+                if rod_count == String::from("0") && run_info.split == Split::SecondStructure {
+                    rod_count = String::from("1+");
+                }
+            }
+            if item_data_content == "" {
+                item_data_content = format!("{} {}", ROD_EMOJI, rod_count);
+            } else {
+                item_data_content = format!("{}  {} {}", item_data_content, ROD_EMOJI, rod_count);
+            }
+            if item_data_content == "" {
+                item_data_content = format!("{} {}", PEARL_EMOJI, pearl_count);
+            } else {
+                item_data_content =
+                    format!("{}  {} {}", item_data_content, PEARL_EMOJI, pearl_count);
+            }
+        }
+        None => {
+            item_data_content = format!("{} {}", ROD_EMOJI, 0);
+            item_data_content = format!("{}  {} {}", item_data_content, PEARL_EMOJI, 0);
+        }
+    }
+    let metadata = format!(
+        "{} {} - {} {}",
+        live_indicator,
+        format_time(last_event.igt as u64),
+        split_desc,
+        response.nickname.replace("_", SPECIAL_UNDERSCORE)
+    );
+
+    let ping_content = format!(
+        "{}\n-# {}",
+        metadata.clone(),
+        roles_to_ping
+            .iter()
+            .map(|role| role.guild_role.mention().to_string())
+            .collect::<Vec<_>>()
+            .join(" "),
+    );
+
+    let pace_content = format!(
+        "{}  {} - {}",
+        split_emoji,
+        format_time(last_event.igt as u64),
+        split_desc,
+    );
+
+    match guild_data
+        .pace_channel
+        .send_message(&ctx, |m| {
+            m.embed(|e| {
+                e.set_author(author.clone());
+                e.field(pace_content.clone(), "", false);
+                if response.user.live_account.is_some() {
+                    e.field(format!("{} {}", TWITCH_EMOJI, live_link.clone()), "", false);
+                } else {
+                    e.field(format!("{}  Offline", OFFLINE_EMOJI), "", false);
+                }
+                e.field("Splits", format!("[Link]({})", stats_link.clone()), true);
+                e.field(
+                    "Time",
+                    format!("<t:{}:R>", (response.last_updated / 1000) as u64),
+                    true,
+                );
+                e.field("Items", item_data_content.clone(), true);
+                if let RunType::Bastionless = run_info.run_type {
+                    e.field("Bastionless", "Yes", true);
+                }
+                e
+            })
+            .content(ping_content.to_owned())
+        })
+        .await
+    {
+        Ok(mut message) => {
+            println!(
+                "Sent pace-ping for user with name: '{}' for split: '{}' in guild name: {}.",
+                response.nickname, split_desc, guild_data.name
+            );
+            let removable_roles = roles_to_ping
+                .iter()
+                .filter(|r| r.runner.as_str() != "")
+                .map(|r| r.guild_role.mention())
+                .collect::<Vec<_>>();
+            let mut new_content = ping_content.to_owned();
+            for role in removable_roles {
+                let replacable_str = format!("{} ", role);
+                new_content = new_content.replace(replacable_str.as_str(), "");
+            }
+            let content_removed_metadata =
+                new_content.replace(format!("{}\n", metadata).as_str(), "");
+            match message
+                .edit(&ctx.http, |m| {
+                    m.embed(|e| {
+                        e.set_author(author);
+                        e.field(pace_content, "", false);
+                        if response.user.live_account.is_some() {
+                            e.field(format!("{} {}", TWITCH_EMOJI, live_link.clone()), "", false);
+                        } else {
+                            e.field(format!("{}  Offline", OFFLINE_EMOJI), "", false);
+                        }
+                        e.field("Splits", format!("[Link]({})", stats_link), true);
+                        e.field(
+                            "Time",
+                            format!("<t:{}:R>", (response.last_updated / 1000) as u64),
+                            true,
+                        );
+                        e.field("Items", item_data_content, true);
+                        if let RunType::Bastionless = run_info.run_type {
+                            e.field("Bastionless", "Yes", true);
+                        }
+                        e
+                    })
+                    .content(content_removed_metadata)
+                })
+                .await
+            {
+                Ok(_) => (),
+                Err(err) => {
+                    return eprintln!("HandlePaceEvent: edit message due to: {}", err);
+                }
+            };
+        }
+        Err(err) => {
+            return eprintln!(
+                "HandlePaceEvent: send split: '{}' with roles: {:?} due to: {}",
+                split_desc, roles_to_ping, err
+            );
+        }
+    };
+}
