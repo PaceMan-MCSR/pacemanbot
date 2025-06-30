ad70aa6b3bda5aac631e043399a1d3cbb4f04085
arjun <arnair7579@gmail.com>
2025-06-30 11:54:15 +0530

    On branch fix/add-sleep-between-test-splits  Changes to be committed: 	new file:   tests/test-multi-aa.sh 	new file:   tests/test-multi.sh

---
+++ tests/test-multi-aa.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for file in "$@"; do
+  echo "Sending $file to /api/ars/aa/sendEvent"
+  curl -X POST -H "Content-Type: application/json" \
+       -d "$(cat "$file")" \
+       http://localhost:8001/api/ars/aa/sendEvent
+  echo "10 sec sleep"
+  sleep 10
+done
---
+++ tests/test-multi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for file in "$@"; do
+  echo "Sending $file to /api/ars/sendevent"
+  curl -X POST -H "Content-Type: application/json" \
+       -d "$(cat "$file")" \
+       http://localhost:8001/api/ars/sendevent
+  echo "10 sec sleep"
+  sleep 10
+done
