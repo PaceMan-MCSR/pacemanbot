3f4cb7e7ba990747793f1c5b406f51ff4d579cfd
arjun <arnair7579@gmail.com>
2025-06-30 21:15:05 +0530

    On branch fix/add-sleep-between-test-splits  Changes to be committed: 	modified:   .github/workflows/ci_17.yml 	deleted:    tests/test-multi-aa.sh 	deleted:    tests/test-multi.sh

--- tests/test-multi-aa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in "$@"; do
-  echo "Sending $file to /api/ars/aa/sendEvent"
-  curl -X POST -H "Content-Type: application/json" \
-       -d "$(cat "$file")" \
-       http://localhost:8001/api/ars/aa/sendEvent
-  sleep 10
-done
--- tests/test-multi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in "$@"; do
-  echo "Sending $file to /api/ars/sendevent"
-  curl -X POST -H "Content-Type: application/json" \
-       -d "$(cat "$file")" \
-       http://localhost:8001/api/ars/sendevent
-  sleep 10
-done
