24e051b0ca18beb3ddf260fd6e1508b3d9c29351
itsdxrk <28788874+itsdxrk@users.noreply.github.com>
2026-03-18 17:19:21 +0000

    cargo fmt

--- src/utils/get_event_type.rs
+++ src/utils/get_event_type.rs
@@ -2,7 +2,9 @@ use crate::ws::response::{Event, EventId, EventType};
 
 pub fn get_event_type(last_event: &Event) -> Option<EventType> {
     match last_event.event_id {
-        EventId::RsgTowerStart | EventId::RsgFindingStronghold | EventId::RsgEnterEnd => Some(EventType::PaceEvent),
+        EventId::RsgTowerStart | EventId::RsgFindingStronghold | EventId::RsgEnterEnd => {
+            Some(EventType::PaceEvent)
+        }
         EventId::RsgCredits => Some(EventType::NonPaceEvent),
         _ => None,
     }
