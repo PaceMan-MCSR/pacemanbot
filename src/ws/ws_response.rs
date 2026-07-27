7b4742c1a8b6d8d409d9b5aa4972df3b1bcd966d
its-saanvi <saanviwastaken544@gmail.com>
2026-07-27 12:04:14 +0530

    fix: unused fields causing parse errors

--- src/ws/ws_response.rs
+++ src/ws/ws_response.rs
@@ -22,21 +22,21 @@ pub struct User {
 #[derive(Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct ItemData {
-    pub _estimated_counts: HashMap<Item, u32>,
-    pub _usages: Option<HashMap<Item, u32>>,
+    pub estimated_counts: HashMap<Item, u32>,
+    pub usages: Option<HashMap<Item, u32>>,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct WSResponse {
     pub game_version: Option<String>,
-    pub _world_id: String,
+    pub world_id: String,
     pub event_list: Vec<Event>,
-    pub _context_event_list: Vec<Event>,
+    pub context_event_list: Vec<Event>,
     pub user: User,
-    pub _is_cheated: bool,
-    pub _is_hidden: bool,
+    pub is_cheated: bool,
+    pub is_hidden: bool,
     pub last_updated: i64,
-    pub _item_data: Option<ItemData>,
+    pub item_data: Option<ItemData>,
     pub nickname: String,
 }
